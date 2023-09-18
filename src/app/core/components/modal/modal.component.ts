8e84bf81dbce1159028dbc2e874678f8d31fa63f
onzomarco <marco.onzo@k-sport.tech>
2023-09-18 18:05:38 +0200

    feat(): added exit animation to modal

--- src/app/core/components/modal/modal.component.ts
+++ src/app/core/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, EventEmitter, Input, Output, TemplateRef, ViewChild} from '@angular/core';
 import {ModalContainerDirective} from "./modal-container.directive";
+import {BaseCardComponent} from "../../../uikit/components/base-card/base-card.component";
 
 @Component({
   selector: 'eurekax-modal',
@@ -14,6 +15,7 @@ export class ModalComponent {
   @Input() size: 'sm' | 'md' | 'lg' = 'lg';
   @Output() closeEvent: EventEmitter<void> = new EventEmitter<void>();
   @ViewChild(ModalContainerDirective, {static: true}) modalDirective!: ModalContainerDirective;
+  protected id = 'modal-' + Date.now();
 
   constructor(private elementRef: ElementRef) {
   }
@@ -21,8 +23,11 @@ export class ModalComponent {
 
   close(backdrop?: boolean) {
     if(!backdrop || (backdrop && this.backdropDismiss)) {
-      this.elementRef.nativeElement.remove();
-      this.closeEvent.emit()
+      document.getElementById(this.id)?.classList.add(this.position === 'bottom' ? 'fade-out-bottom' : 'fade-out-top');
+      setTimeout(() => {
+        this.elementRef.nativeElement.remove();
+        this.closeEvent.emit();
+      }, 750)
     }
   }
 
