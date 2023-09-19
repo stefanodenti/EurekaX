5165e2d528171591988e6b36eb2246e0738a8645
onzomarco <marco.onzo@k-sport.tech>
2023-09-19 18:00:56 +0200

    fix()

--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -21,23 +21,26 @@ export class ModalService {
   openModal<T>(content: TemplateRef<any> | Type<T>, options?: {title?: string, showCloseButton?: boolean, backdropDismiss?: boolean, position?: 'center' | 'top' | 'bottom', size?: 'sm' | 'md' | 'lg'} ) {
     let modalComponent;
     if(content instanceof TemplateRef) {
+      const embeddedView = content.createEmbeddedView(null);
       modalComponent = createComponent(ModalComponent,
         {environmentInjector: this.applicationRef.injector,
-        projectableNodes: [content.createEmbeddedView(null).rootNodes]});
+          projectableNodes: [embeddedView.rootNodes]});
+      this.applicationRef.attachView(embeddedView);
     } else {
       modalComponent = createComponent(ModalComponent, {environmentInjector: this.applicationRef.injector});
       const componentViewRef = modalComponent.instance.modalDirective.viewContainerRef;
       componentViewRef.clear();
-      componentViewRef.createComponent(content);
+      const component = componentViewRef.createComponent(content);
+      component.hostView.detectChanges();
     }
-
-      modalComponent.instance.title = options?.title;
-      modalComponent.instance.showCloseButton = options?.showCloseButton ?? true;
-      modalComponent.instance.backdropDismiss = options?.backdropDismiss ?? true;
-      modalComponent.instance.position = options?.position ?? 'center';
-      modalComponent.instance.size = options?.size ?? 'lg';
-      modalComponent.hostView.detectChanges();
-      this.document.body.appendChild(modalComponent.location.nativeElement);
-      return modalComponent.instance;
+    modalComponent.instance.title = options?.title;
+    modalComponent.instance.showCloseButton = options?.showCloseButton ?? true;
+    modalComponent.instance.backdropDismiss = options?.backdropDismiss ?? true;
+    modalComponent.instance.position = options?.position ?? 'center';
+    modalComponent.instance.size = options?.size ?? 'lg';
+    modalComponent.hostView.detectChanges();
+    modalComponent.changeDetectorRef.detectChanges();
+    this.document.body.appendChild(modalComponent.location.nativeElement);
+    return modalComponent.instance;
   }
 }
